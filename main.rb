e1864e7b4a888b0b1879f84d31bba665aa68774f
huequica <harumaki1225@gmail.com>
2019-03-06 13:05:52 +0900

    mainメソッド削除、モジュール化した

--- main.rb
+++ main.rb
@@ -1,5 +1,3 @@
-require 'miyabi'
-require 'romkan'
 # for debug
 class Debug
   def initialize(constant_value)
@@ -17,105 +15,85 @@ class Debug
   end
 end
 
-################ main method #################
+# convert
+class Unzen_lang
+  require 'miyabi'
+  require 'romkan'
+  def initialize; end
 
-Single_Replace_str = { '0' => %w[m ai], # 0
-                       '1' => %w[i u],  # 1
-                       '2' => %w[n o],  # 2
-                       '3' => %w[sh a], # 3
-                       '4' => %w[ch e], # 4
-                       '5' => %w[j i],  # 5
-                       '6' => %w[z yu], # 6
-                       '7' => %w[d yo], # 7
-                       '8' => %w[t ya], # 8
-                       '9' => %w[ts ie] }.freeze # 9
-# 0 : m, ai
-# 1 : i, u
-# 2 : n, o
-# 3 : sh, a
-# 4 : ch, e
-# 5 : j, i
-# 6 : z, yu
-# 7 : d, yo
-# 8 : t, ya
-# 9 : ts, ie
-Double_Replace_str = { '00' => 'p', # 00
-                       '11' => 'x', # 11
-                       '22' => 'h', # 22
-                       '33' => 's', # 33
-                       '44' => 'k', # 44
-                       '55' => 'w', # 55
-                       '66' => 'g', # 66
-                       '77' => 'f', # 77
-                       '88' => 'b', # 88
-                       '99' => 'r' }.freeze # 99
-# 00 : p
-# 22 : h
-# 33 : s
-# 44 : k
-# 55 : v
-# 66 : g
-# 77 : f
-# 88 : b
-# 99 : r
+  Single_Replace_str = { '0' => %w[m ai], # 0
+                         '1' => %w[i u],  # 1
+                         '2' => %w[n o],  # 2
+                         '3' => %w[sh a], # 3
+                         '4' => %w[ch e], # 4
+                         '5' => %w[j i],  # 5
+                         '6' => %w[z yu], # 6
+                         '7' => %w[d yo], # 7
+                         '8' => %w[t ya], # 8
+                         '9' => %w[ts ie] }.freeze # 9
+  Double_Replace_str = { '00' => 'p', # 00
+                         '11' => 'x', # 11
+                         '22' => 'h', # 22
+                         '33' => 's', # 33
+                         '44' => 'k', # 44
+                         '55' => 'w', # 55
+                         '66' => 'g', # 66
+                         '77' => 'f', # 77
+                         '88' => 'b', # 88
+                         '99' => 'r' }.freeze # 99
+  attr_reader :Single_Replace_str
+  attr_reader :Double_Replace_str
 
-# debug = Debug.new(Single_Replace_str)
-# debug.div
-# puts ''
-# debug = Debug.new(Double_Replace_str)
-# debug.div
+  def convert(convert_text)
+    begin
+      convert_text = convert_text.to_kanhira.to_hira.to_roma
+    rescue Mechanize::ResponseCodeError
+      convert_text = convert_text.to_roma
+    end
+    return_str = ''
 
+    while convert_text != ''
+      # 見つからなかったときのためのフラグ
+      notfound_flag = true
 
-convert_text = ARGV[0]
-begin
-  convert_text = convert_text.to_kanhira.to_hira.to_roma
-rescue Mechanize::ResponseCodeError
-  convert_text = convert_text.to_roma
-end
-return_str = ''
+      # 2つ文字を先に判定させてあった場合脱出
+      Double_Replace_str.each do |str|
+        next unless convert_text[0] == str[1]
 
-while convert_text != ''
-  # 見つからなかったときのためのフラグ
-  notfound_flag = true
+        convert_text.slice!(0)
+        return_str += str[0]
+        notfound_flag = false
+        break
+      end
 
-  # 2つ文字を先に判定させてあった場合脱出
-  Double_Replace_str.each do |str|
-    next unless convert_text[0] == str[1]
+      Single_Replace_str.each do |str|
+        str0_length = str[1][0].size - 1
+        str1_length = str[1][1].size - 1
 
-    convert_text.slice!(0)
-    return_str += str[0]
-    notfound_flag = false
-    break
-    # Double_Replace_str.each
-  end
+        if convert_text[0..str0_length] == str[1][0]
+          convert_text.slice!(0..str0_length)
+          return_str += str[0]
+          notfound_flag = false
 
-  Single_Replace_str.each do |str|
-    str0_length = str[1][0].size - 1
-    str1_length = str[1][1].size - 1
-    if convert_text[0..str0_length] == str[1][0]
+        elsif convert_text[0..str1_length] == str[1][1]
+          convert_text.slice!(0..str1_length)
+          return_str += str[0]
+          notfound_flag = false
+        end
+      end
 
-      convert_text.slice!(0..str0_length)
-      return_str += str[0]
-      notfound_flag = false
-
-    elsif convert_text[0..str1_length] == str[1][1]
-
-      convert_text.slice!(0..str1_length)
-      return_str += str[0]
-      notfound_flag = false
+      if notfound_flag
+        if convert_text[0] != "'"
+          return_str += convert_text[0]
+          convert_text.slice!(0)
+          notfound_flag = false
+        else
+          puts "#{convert_text[0]}が見つからなかったので飛ばしました"
+          convert_text.slice!(0)
+        end
+      end
 
     end
-  end
-
-  if notfound_flag
-    if convert_text[0] != "'"
-      return_str += convert_text[0]
-      convert_text.slice!(0)
-      notfound_flag = false
-    else
-      puts "#{convert_text[0]}が見つからなかったので飛ばしました"
-      convert_text.slice!(0)
-    end
+    return_str
   end
 end
-puts return_str
